0614670820b5ef20fa7ab5f3102d34b96558f519
seanluse41 <seanluse41@gmail.com>
2023-02-27 23:06:49 +0900

    fix some file paths

--- Ocean.gd
+++
@@ -1,7 +0,0 @@
-@tool
-
-extends Node3D
-
-# Get waterplane and preset grid spawn info
-var OceanTile = preload("res://Scenes/WaterPlane.tscn"); 
-var spawnPoint = preload("res://Resources/GridSpawnInfo.tres");
